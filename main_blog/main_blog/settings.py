6a33681ccc8749df0d73a1943c3e88bacb5ba4fc
Moataz Fawzy <142129253+Moataz0000@users.noreply.github.com>
2024-05-01 04:22:45 +0300

    Update settings.py

--- main_blog/main_blog/settings.py
+++ main_blog/main_blog/settings.py
@@ -135,8 +135,8 @@ MEDIA_URL = '/media/'
 
 # Simple Mail Transfer Portcol
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "motazfawzy73@gmail.com"
-EMAIL_HOST_PASSWORD = "budl mngo txcp fhrg"
+EMAIL_HOST_USER = "your E-mail"
+EMAIL_HOST_PASSWORD = "your password"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
